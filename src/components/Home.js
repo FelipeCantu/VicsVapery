6e380d2d23dbf8e7704fd94768e18d71bb37222b
FelipeCantu <felipecantujr@Felipes-MBP.lan>
2023-06-19 21:16:34 -0500

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
+import MainImg from '../assets/mainim.png'
+import Elibut from '../assets/elibut.png'
+import Dispbut from '../assets/dispbut.gif'
 
 function Home() {
   return (
     <Main>
-      <h1>Hello</h1>
+      <hr />
+      <p>
+          graegkjrdaklfgjadrl;kg graegkjrdaklfgjadrl
+          graegkjrdaklfgjadrlx graegkjrdaklfgjadrl f graegkjrdaklfgjadrl graegkjrdaklfgjadrl graegkjrdaklfgjadrl
+          graegkjrdaklfgjadrlgraegkjrdaklfgjadrlgraegkjrdaklfgjadrlgraegkjrdaklfgjadrlgraegkjrdaklfgjadrl
+          
+        </p>
+        <img src={MainImg} width="700px" height="400px" />
+        <img src={Elibut} width="700px" height="400px" />
+        <img src={Dispbut} width="700px" height="400px" />
+        <p>
+          graegkjrdaklfgjadrl;kg graegkjrdaklfgjadrl
+          graegkjrdaklfgjadrlx graegkjrdaklfgjadrl f graegkjrdaklfgjadrl graegkjrdaklfgjadrl graegkjrdaklfgjadrl
+          graegkjrdaklfgjadrlgraegkjrdaklfgjadrlgraegkjrdaklfgjadrlgraegkjrdaklfgjadrlgraegkjrdaklfgjadrl
+          
+          v
+        </p>
     </Main>
   )
 }
 
 const Main = styled.div`
-  background: black;
-  color: white;
+  height: 100vh;
+  overflow: auto;
+  p {
+    font-size: 50px;
+    color: black;
+  }
 `;
 
 
