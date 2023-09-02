4e13b7f70ec1898ed4bba96858946d8a1e1d8523
Felipe Cantu Jr <cantujr.felipe@gmail.com>
2023-09-02 12:38:46 -0500

    404

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import styled from 'styled-components'
 function App() {
   return (
     <div className="App">
-        <Router basename='/Home' >
+        <Router>
           <Navbar />
           <Redirect to="/Home" />
           <Route path="/Home" component={Home} />
